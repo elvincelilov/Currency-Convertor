607593a999db88c27b8c3f7ee30aa6dc955fbd33
elvincelilov <celilovelvin11@gmail.com>
2022-04-22 15:06:14 +0400

    CURRENCY

--- style.js
+++ style.js
@@ -1,136 +1,141 @@
 let right = document.querySelectorAll('.sag');   
-let left = document.querySelectorAll('.sol');   
-let p = document.querySelector('p');   
-let amount2 = document.getElementById('rinput');   
-let amount1 = document.getElementById('linput');  
-let sagm=document.querySelector('.solM')  
-let solm=document.querySelector('.sagM')  
-let soll = document.getElementById('sol')
+let left = document.querySelectorAll('.sol');      
+let rinput = document.getElementById('rinput');   
+let linput = document.getElementById('linput');  
+let leftp=document.getElementById("sol")
+let rightp=document.getElementById("sag")
    
 let sol;   
 let sag;   
    
-amount1.addEventListener('keyup', function(){         
+linput.addEventListener('keyup', function(){         
     getvalue()   
 })    
 
  
 right.forEach(event => {   
     event.addEventListener('click', function(){   
-        sol = this.innerText        
+        sag = this.innerText        
         getvalue()   
     })    
 });   
    
 left.forEach(event => {   
     event.addEventListener('click', function(){   
-        sag = this.innerText   
+        sol = this.innerText   
         getvalue()   
     })    
 });   
    
 const getvalue = async () => {   
     let out;   
-    await fetch(`https:api.exchangerate.host/latest?base=${sag}&symbols=${sol}`)   
+    await fetch(`https:api.exchangerate.host/latest?base=${sol}&symbols=${sag}`)   
     .then(response => response.json())   
     .then((data) => {   
         out = Object.values(data.rates)[0]   
         console.log(data)   
     })  
-    function r(){
-        amount2.addEventListener('keyup', function(){         
-            amount1.value = amount2.value / out
+    function opposite(){
+        rinput.addEventListener('keyup', function(){         
+            linput.value = rinput.value / out
          })  
     }
     
 
 
     if ((sol == 'RUB') && (sag == 'RUB')) {   
-        amount2.value = amount1.value   
-        solm.innerText = '1RUB = ' +out + 'RUB'
-        sagm.innerText = '1RUB = ' + 1/out + 'RUB'
+        rinput.value = linput.value   
+        leftp.innerText = '1 RUB = ' +out + ' RUB'
+        rightp.innerText = '1 RUB = ' + 1/out + ' RUB'
     
     }   
     if ((sol == 'RUB') && (sag == 'EUR')) {   
-        amount2.value = amount1.value * out 
-        solm.innerText = '1RUB = ' +out + 'EUR'
-        sagm.innerText = '1EUR = ' + 1/out + 'RUB'
-        r()
+        rinput.value = linput.value * out 
+        leftp.innerText = '1 RUB = ' + out + ' EUR'
+        rightp.innerText = '1 EUR = ' + 1/out + ' RUB'
+        opposite()
     }   
     if ((sol == 'RUB') && (sag == 'USD')) {  
-        amount2.value = amount1.value * out; 
-        solm.innerText = '1RUB = ' +out + 'USD'
-        sagm.innerText = '1USD = ' + 1/out + 'RUB'
-        r()
+        rinput.value = linput.value * out; 
+        leftp.innerText = '1 RUB = ' +out + ' USD'
+        rightp.innerText = '1 USD = ' + 1/out + ' RUB'
+        opposite()
     }   
     if ((sol == 'RUB') && (sag == 'AZN')) {   
-        amount2.value = amount1.value * out  
-        solm.innerText = '1RUB = ' +out + 'AZN'
-        sagm.innerText = '1AZN = ' + 1/out + 'RUB'
-        r() 
+        rinput.value = linput.value * out  
+        leftp.innerText = '1 RUB = ' +out + ' AZN'
+        rightp.innerText = '1 AZN = ' + 1/out + ' RUB'
+        opposite() 
     }   
     else if ((sol == 'USD') && (sag == 'USD')) {   
-        amount2.value = amount1.value   
-        solm.innerText = '1USD = ' +out + 'USD'
-        sagm.innerText = '1USD = ' + 1/out + 'USD'
+        rinput.value = linput.value   
+        leftp.innerText = '1 USD = ' +out + ' USD'
+        rightp.innerText = '1 USD = ' + 1/out + ' USD'
     }   
     else if ((sol == 'USD') && (sag == 'EUR')) {   
-        amount2.value = amount1.value * out 
-        solm.innerText = '1USD = ' +out + 'EUR'
-        sagm.innerText = '1EUR = ' + 1/out + 'USD' 
-        r() 
+        rinput.value = linput.value * out 
+        leftp.innerText = '1 USD = ' +out + ' EUR'
+        rightp.innerText = '1 EUR = ' + 1/out + ' USD' 
+        opposite() 
     }   
     else if ((sol == 'USD') && (sag == 'RUB')) {   
-        amount2.value = amount1.value * out 
-        solm.innerText = '1USD = ' +out + 'RUB'
-        sagm.innerText = '1RUB = ' + 1/out + 'USD'
-        r()  
+        rinput.value = linput.value * out 
+        leftp.innerText = '1 USD = ' +out + ' RUB'
+        rightp.innerText = '1 RUB = ' + 1/out + ' USD'
+        opposite()  
     }   
     else if ((sol == 'USD') && (sag == 'AZN')) {   
-        amount2.value = amount1.value * out
-        solm.innerText = '1USD = ' +out + 'AZN'
-        sagm.innerText = '1AZN = ' + 1/out + 'USD' 
-        r()  
+        rinput.value = linput.value * out
+        leftp.innerText = '1 USD = ' +out + ' AZN'
+        rightp.innerText = '1 AZN = ' + 1/out + ' USD' 
+        opposite()  
     }   
-    else if ((sol == 'EUR') && (sag == 'EUR')) {   
-        amount2.value = amount1.value   
-        solm.innerText = '1EUR = ' +out + 'EUR'
-        sagm.innerText = '1EUR = ' + 1/out + 'EUR'
+    else if ((sol == ' EUR') && (sag == 'EUR')) {   
+        rinput.value = linput.value   
+        leftp.innerText = '1 EUR = ' + out + ' EUR'
+        rightp.innerText = '1 EUR = ' + 1/out + ' EUR'
     }   
     else if ((sol == 'EUR') && (sag == 'USD')) {   
-        amount2.value = amount1.value * out
-        solm.innerText = '1EUR = ' +out + 'USD'
-        sagm.innerText = '1USD = ' + 1/out + 'EUR' 
-        r()  
+        rinput.value = linput.value * out
+        leftp.innerText = '1 EUR = ' +out + ' USD'
+        rightp.innerText = '1 USD = ' + 1/out + ' EUR' 
+        opposite()  
     }   
     else if ((sol == 'EUR') && (sag == 'RUB')) {   
-        amount2.value = amount1.value * out  
-        solm.innerText = '1EUR = ' +out + 'RUB'
-        sagm.innerText = '1RUB = ' + 1/out + 'EUR' 
-        r()
+        rinput.value = linput.value * out  
+        leftp.innerText = '1 EUR = ' +out + ' RUB'
+        rightp.innerText = '1 RUB = ' + 1/out + ' EUR' 
+        opposite()
     }   
     else if ((sol == 'EUR') && (sag == 'AZN')) {   
-        amount2.value = amount1.value * out 
-        solm.innerText = '1EUR = ' +out + 'AZN'
-        sagm.innerText = '1AZN = ' + 1/out + 'EUR'  
-        r()
+        rinput.value = linput.value * out 
+        leftp.innerText = '1 EUR = ' +out + ' AZN'
+        rightp.innerText = '1 AZN = ' + 1/out + ' EUR'  
+        opposite()
     }   
     else if ((sol == 'AZN') && (sag == 'AZN')) {   
-        amount2.value = amount1.value   
-        solm.innerText = '1AZN = ' +out + 'AZN'
-        sagm.innerText = '1AZN = ' + 1
+        rinput.value = linput.value   
+        leftp.innerText = '1 AZN = ' +out + ' AZN'
+        rightp.innerText = '1 AZN = ' +1/out + ' AZN'
+        opposite()
     }   
     else if ((sol == 'AZN') && (sag == 'RUB')) {   
-        amount2.value = amount1.value * out;  
-        solm.innerText = '1AZN = ' +out + 'RUB'
-        sagm.innerText = '1RUB = ' + 1/out + 'AZN' 
-        r()
-    }   
+        rinput.value = linput.value * out;  
+        leftp.innerText = '1 AZN = ' +out + ' RUB'
+        rightp.innerText = '1 RUB = ' + 1/out + ' AZN' 
+        opposite()
+    }
+    else if ((sol == 'AZN') && (sag == 'EUR')) {   
+        rinput.value = linput.value * out;  
+        leftp.innerText = '1 AZN = ' +out + ' EUR'
+        rightp.innerText = '1 EUR = ' + 1/out + ' AZN' 
+        opposite()
+    }    
     else if ((sol == 'AZN') && (sag == 'USD')) {   
-        amount2.value = amount1.value * out   
-        solm.innerText = '1AZN = ' +out + 'USD'
-        sagm.innerText = '1USD = ' + 1/out + 'AZN'
-        r()
+        rinput.value = linput.value * out   
+        leftp.innerText = '1 AZN = ' +out + ' USD'
+        rightp.innerText = '1 USD = ' + 1/out + ' AZN'
+        opposite()
     }   
 }   
    
